89b46b4dd956962d479c094d07e5e59059d5337a
itangishaka1 <abdullahitangishaka@gmail.com>
2022-08-31 16:11:01 -0400

    Styling our App. Added Card and Avatar components also

---
+++ client/src/shared/UIElements/Card/Card.jsx
@@ -0,0 +1,12 @@
+
+import './Card.scss';
+
+const Card = props => {
+  return (
+    <div className={`card ${props.className}`} style={props.style}>
+      {props.children}
+    </div>
+  );
+};
+
+export default Card;
--- client/src/user/components/UserList/UserItem.jsx
+++ client/src/user/components/UserList/UserItem.jsx
@@ -1,17 +1,24 @@
+import { Link } from "react-router-dom"
 import "./UserItem.scss"
+import Avatar from "../../../shared/UIElements/Avatar/Avatar"
+import Card from "../../../shared/UIElements/Card/Card"
 
 const UserItem = ({ id, image, name, placeCount }) => {
   return (
     <li className="userItem">
-      <div className="userItem__content">
-        <div className="userItem__image">
-          <img src={image} alt={name}  />
-        </div>
-        <div className="userItem__info">
-          <h2>{name}</h2>
-          <h3>{placeCount} {placeCount === 1 ? ' place' : ' places'} </h3>
-        </div>
-      </div>
+      <Card className="userItem__content">
+        <Link to={`/${id}/places`}>
+          <div className="userItem__image">
+            <Avatar image={image} alt={name} />
+          </div>
+          <div className="userItem__info">
+            <h2>{name}</h2>
+            <h3>
+              {placeCount} {placeCount === 1 ? " place" : " places"}{" "}
+            </h3>
+          </div>
+        </Link>
+      </Card>
     </li>
   )
 }

72b5bf4183c700d915fab1301a78ea0980884b2b
itangishaka1 <abdullahitangishaka@gmail.com>
2022-09-01 13:03:42 -0400

    deleted the src/shared/UIElements/react-frontend-10-animation folder and Added a coment in SideDrawer component for the use of react-transition-group

--- client/src/shared/components/Navigation/SideDrawer.jsx
+++ client/src/shared/components/Navigation/SideDrawer.jsx
@@ -7,7 +7,7 @@ const SideDrawer = props => {
     <CSSTransition
       in={props.show}
       timeout={200}
-      classNames="slide-in-left"
+      classNames = "slide-in-left" //! Attention: classNames (with 's')
       mountOnEnter
       unmountOnExit
     >
